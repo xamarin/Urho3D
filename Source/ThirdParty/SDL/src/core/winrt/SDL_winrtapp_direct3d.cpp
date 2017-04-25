45f5b956a74730904af99516f10f6b14cefe71a9
EgorBo <egorbo@gmail.com>
2017-04-26 02:02:43 +0300

    Fix compilation errors

--- Source/ThirdParty/SDL/src/core/winrt/SDL_winrtapp_direct3d.cpp
+++ Source/ThirdParty/SDL/src/core/winrt/SDL_winrtapp_direct3d.cpp
@@ -822,9 +822,12 @@ static void WINRT_OnBackButtonPressed(BackButtonEventArgs ^ args)
     SDL_SendKeyboardKey(SDL_PRESSED, SDL_SCANCODE_AC_BACK);
     SDL_SendKeyboardKey(SDL_RELEASED, SDL_SCANCODE_AC_BACK);
 
-    if (SDL_GetHintBoolean(SDL_HINT_WINRT_HANDLE_BACK_BUTTON, SDL_FALSE)) {
-        args->Handled = true;
-    }
+     const char *hint = SDL_GetHint(SDL_HINT_WINRT_HANDLE_BACK_BUTTON);
+     if (hint) {
+         if (*hint == '1') {
+             args->Handled = true;
+         }
+      }
 }
 
 #if WINAPI_FAMILY != WINAPI_FAMILY_PHONE_APP
--- Source/ThirdParty/SDL/src/core/winrt/SDL_winrtapp_direct3d.h
+++ Source/ThirdParty/SDL/src/core/winrt/SDL_winrtapp_direct3d.h
@@ -75,6 +75,7 @@ protected:
     void OnCharacterReceived(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CharacterReceivedEventArgs^ args);
 
 #if NTDDI_VERSION >= NTDDI_WIN10
+    void OnGamepadAdded(Platform::Object ^sender, Windows::Gaming::Input::Gamepad ^gamepad);
     void OnBackButtonPressed(Platform::Object^ sender, Windows::UI::Core::BackRequestedEventArgs^ args);
 #elif WINAPI_FAMILY == WINAPI_FAMILY_PHONE_APP
     void OnBackButtonPressed(Platform::Object^ sender, Windows::Phone::UI::Input::BackPressedEventArgs^ args);
